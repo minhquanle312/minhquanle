f107604929b4293b1fb8ac50b089c4f34112557e
Minh Quan Le <minhquanle312@gmail.com>
2022-04-16 19:47:13 +0700

    add form submit

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 import styles from '../styles/Contact.module.css'
 import Link from 'next/link'
@@ -69,6 +69,39 @@ const Contact = () => {
     </div>
   )
 
+  const emailInputRef = useRef()
+  const nameInputRef = useRef()
+  const subjectInputRef = useRef()
+  const messageInputRef = useRef()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    const emailValue = emailInputRef.current?.value
+    const nameValue = nameInputRef.current?.value
+    const subjectValue = subjectInputRef.current?.value
+    const messageValue = messageInputRef.current?.value
+
+    console.log(emailValue)
+    console.log(nameValue)
+    console.log(subjectValue)
+    console.log(messageValue)
+
+    fetch('https://formsubmit.co/ajax/minhquanle312@gmail.com', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        email: emailValue,
+        name: nameValue,
+        subject: subjectValue,
+        message: messageValue,
+      }),
+    }).then(response => response.json())
+  }
+
   return (
     <div id='contact' className='pb-28 pt-16 bg-blue-50'>
       <h2 className='text-center text-5xl mb-14'>Contact</h2>
@@ -82,32 +115,36 @@ const Contact = () => {
         </div>
         <div className='flex flex-col'>
           <h2 className='text-center text-3xl mb-10'>Mail to me</h2>
-          <form
-            action='https://formsubmit.co/minhquanle312@gmail.com'
-            method='POST'
-            className='flex flex-col gap-3'
-          >
+          <form className='flex flex-col gap-3'>
             <input
               type='email'
               placeholder='Email'
               className='reset-input'
               required
+              ref={emailInputRef}
             />
             <input
               type='text'
               placeholder='Name'
               className='reset-input'
               required
+              ref={nameInputRef}
             />
-            <input type='text' placeholder='Subject' className='reset-input' />
+            <input
+              ref={subjectInputRef}
+              type='text'
+              placeholder='Subject'
+              className='reset-input'
+            />
             <textarea
               required
               type='text'
               placeholder='Message'
               maxLength='1000'
               className='reset-input min-h-[100px] max-h-96'
+              ref={messageInputRef}
             />
-            <button type='submit' className='btn-primary self-start'>
+            <button className='btn-primary self-start' onClick={handleSubmit}>
               Send
             </button>
           </form>
