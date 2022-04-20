dc8a6493194b5d2ccd2808648ea743df3fad23c9
Minh Quan Le <minhquanle312@gmail.com>
2022-04-20 17:00:00 +0700

    update CV

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import Icon from './../components/Icon'
 import React, { useState, useEffect } from 'react'
+import Head from 'next/head'
 
 const Home = () => {
   const [theme, setTheme] = useState(() => {
@@ -30,24 +31,29 @@ const Home = () => {
   }
 
   return (
-    <div className='overflow-x-hidden'>
-      <Header />
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-      <button
-        type='button'
-        onClick={handleClickDarkMode}
-        className='z-10 p-2 fixed bottom-7 left-7 bg-slate-200 border-[3px] border-slate-600 rounded-full shadow-sm fill-orange-400 hover:rotate-45 transition-transform duration-300'
-      >
-        <Icon
-          iconName={theme === 'dark' ? 'moon' : 'sun'}
-          width='7'
-          height='7'
-        />
-      </button>
-    </div>
+    <>
+      <Head>
+        <title>Minh Quân Lê</title>
+      </Head>
+      <div className='overflow-x-hidden'>
+        <Header />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <button
+          type='button'
+          onClick={handleClickDarkMode}
+          className='z-10 p-2 fixed bottom-7 left-7 bg-slate-200 border-[3px] border-slate-600 rounded-full shadow-sm fill-orange-400 hover:rotate-45 transition-transform duration-300'
+        >
+          <Icon
+            iconName={theme === 'dark' ? 'moon' : 'sun'}
+            width='7'
+            height='7'
+          />
+        </button>
+      </div>
+    </>
   )
 }
 
